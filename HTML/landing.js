ba60dd38eeaea2c32decc6dc638e7556247c2bf7
Rachel <rachelnteran@gmail.com>
2024-05-30 18:43:33 -0400

    updating html with api

--- HTML/landing.js
+++ HTML/landing.js
@@ -1,3 +1,5 @@
+const url = "http://127.0.0.1:5000/api/v1.0/"
+
 let features;
 document.getElementById("submit").onclick = function () {
     tmax = document.getElementById("tmax").value;
@@ -7,4 +9,10 @@ document.getElementById("submit").onclick = function () {
     pressure = document.getElementById("pressure").value;
     data = {"tmax":tmax, "tmin":tmin, "humidity":humidity, "wind":wind, "pressure":pressure};
     console.log(data);
+    data_call = `${url}/${tmin}/${tmax}/${humidity}/${wind}/${pressure}`
+    d3.json(data_call).then(function(data){
+        alert(data)
+    });
+
+  
 }
