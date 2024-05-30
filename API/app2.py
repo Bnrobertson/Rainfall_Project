42909db85315f25d700a5ee97666e6c40a8d5ef1
downleft <downleft@jodys-air.attlocal.net>
2024-05-30 18:39:15 -0400

    Test app.py file complete

--- API/app2.py
+++ API/app2.py
@@ -1,9 +1,10 @@
 # Import the dependencies.
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
 import numpy as np
 # from tensorflow.keras.applications.vgg16 import preprocess_input
 import os
+import pickle
 from flask_cors import CORS, cross_origin
 
 
@@ -13,8 +14,11 @@ from flask_cors import CORS, cross_origin
 ### Change model name to load
 ### Seriously, do this!
 app = Flask(__name__)
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-model = load_model('../Neural_Network_Model/rainfall_model.h5', compile = False)
+
+# Load the trained model
+with open('../Neural_Network_Model/rainfall_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+#model = load_model('../Neural_Network_Model/rainfall_model.h5', compile = False)
 CORS(app)
 
 #################################################
@@ -31,15 +35,10 @@ def welcome():
 @app.route("/api/v1.0/<tmin>/<tmax>/<humidity>/<wind>/<pressure>")
 def predict(tmin, tmax, humidity, wind, pressure):
 
-    #if request.method == ["POST"]:
-    
-    result = model.predict(np.array([[float(tmin), float(tmax), float(humidity), float(wind), float(pressure)]]))
-    # if int(result[0])==1:
-    #     prediction = "It is going to rain today"
-    # else:
-    #     prediction = "It is not going to rain today"
-    # return prediction
-    return jsonify(result.tolist())
-
-if __name__ == '__main__':
-    app.run(debug = True)
+    if request.method == "POST":
+        result = model.predict([tmin, tmax, humidity, wind, pressure])
+        if int(result[0])==1:
+            prediction = "It is going to rain today"
+        else:
+            prediction = "It is not going to rain today"
+        return prediction
