6ec8e5bb3ecf3f9a60ceaef4f541625f7538c2a1
Rachel <rachelnteran@gmail.com>
2024-05-30 21:03:22 -0400

    update landing2

--- HTML/landing2.js
+++ HTML/landing2.js
@@ -16,11 +16,11 @@ const url = "http://127.0.0.1:5000/api/v1.0/"
 // d3.select("#submit").on("click", inputfunc)
 let button = d3.select("#submit")
 button.on("click", function () {
-    let tmax = d3.select("tmax").html;
-    let tmin = d3.select("#tmin").html;
-    let humidity = d3.select("#humidity").html;
-    let wind = d3.select("#wind").html;
-    let pressure = d3.select("#pressure").html;
+    let tmax = d3.select("#tmax").property("value");
+    let tmin = d3.select("#tmin").property("value");
+    let humidity = d3.select("#humidity").property("value");
+    let wind = d3.select("#wind").property("value");
+    let pressure = d3.select("#pressure").property("value");
     let data_call = `${url}${tmin}/${tmax}/${humidity}/${wind}/${pressure}`
     d3.json(data_call).then(function (data1) {
         console.log(data1);
